e09dd2e01dbb5bcac8a8b6f0435d8215d7eed5a0
Josh Coffman <joshuaTCoffman@hotmail.com>
2022-10-21 13:18:04 -0500

    add new champion icon component and clean up code - JC

---
+++ src/components/championIcon/championIcon.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface IconProps {
+    championId: string,
+    index: React.Key,
+}
+
+export function ChampionIcon(props: IconProps) {
+    const {championId, index} = props
+
+    return (
+            <div key={index}><img width={125} height={125} src={`http://ddragon.leagueoflegends.com/cdn/12.20.1/img/champion/${championId}.png`} /></div>
+    )
+}
--- src/components/championScrollBar/championScrollBar.tsx
+++ src/components/championScrollBar/championScrollBar.tsx
@@ -1,21 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useService } from '../../hooks/useService';
 import { getChampionList } from '../../services/championService';
+import { ChampionIcon } from '../championIcon/championIcon';
 import './championScrollBar.css';
 
 function ChampionScrollBar() {
   const championList: any = useService(getChampionList).data
 
   return (
-    <>
-      {
         <div className='icon-container'>
           {
-            championList && championList.map((champ: any, index: React.Key) => {return <div key={index}><img width={125} height={125} src={`http://ddragon.leagueoflegends.com/cdn/12.20.1/img/champion/${champ.id}.png`} /></div>})
+            championList && championList.map((champion: any, index: React.Key) => {return <ChampionIcon championId={champion.id} index={index}/>})
           }
         </div>
-      }
-    </>
   );
 }
 
