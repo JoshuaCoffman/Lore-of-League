83d81314e9c45b61f510220524cedb30c8838ea3
Josh Coffman <joshuaTCoffman@hotmail.com>
2023-01-04 15:10:28 -0600

    WIP styling for champion view - learning new ways to use css like grid - JC

--- src/components/championView/championView.tsx
+++ src/components/championView/championView.tsx
@@ -14,8 +14,14 @@ export function ChampionView(props: ChampionViewProps) {
             <img src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${selectedChampion}_0.jpg`} alt="Champion Splash"></img>
             {championInfo &&
                 <>
-                    <div>{championInfo[0].id}</div>
-                    <div>Lore: {championInfo[0].lore}</div>
+                    <div className="name-container">
+                        <div className="name-rhombus"></div>
+                        <div className="name">{championInfo[0].id}</div>
+
+                        {/* <div className="lore-rhombus"></div> */}
+                        {/* <div className="lore">Lore: {championInfo[0].lore}</div> */}
+                    </div>
+                    <div className="lore">Lore: {championInfo[0].lore}</div>
                 </>
             }
 
